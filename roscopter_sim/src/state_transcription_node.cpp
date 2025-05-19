90cfdb34bb935345090aca93d0f68260c1345e41
Jacob Moore <jacobmoor2@gmail.com>
2025-05-19 14:20:56 -0600

    bug: cleaned up roscopter_sim CMakeLists and fixed roscopter_sim transcription node

--- roscopter_sim/src/state_transcription_node.cpp
+++ roscopter_sim/src/state_transcription_node.cpp
@@ -1,116 +1,114 @@
 #include <memory>
 
-#include "Eigen/Geometry"
-#include "geometry_msgs/msg/vector3_stamped.hpp"
-#include "nav_msgs/msg/odometry.hpp"
-#include "rclcpp/rclcpp.hpp"
+#include <Eigen/Geometry>
+#include <geometry_msgs/msg/vector3_stamped.hpp>
+#include <rclcpp/rclcpp.hpp>
+
 #include "roscopter_msgs/msg/state.hpp"
+#include "rosflight_msgs/msg/sim_state.hpp"
 
-#include "chrono"
+namespace roscopter_sim {
 
-using namespace std::chrono_literals;
-using roscopter_msgs::msg::State;
-using std::placeholders::_1;
-
-class gazebo_transcription : public rclcpp::Node
+class SimStateTranscription : public rclcpp::Node
 {
 public:
-  gazebo_transcription()
-      : Node("gazebo_state")
+  SimStateTranscription()
+      : Node("roscopter_state_transcription")
   {
-    odom_subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
-      "/multirotor/truth/NED", 10, std::bind(&gazebo_transcription::publish_truth, this, _1));
+    sim_state_sub_ = this->create_subscription<rosflight_msgs::msg::SimState>(
+      "sim/truth_state", 10, std::bind(&SimStateTranscription::publish_truth, this, std::placeholders::_1));
+    wind_truth_sub_ = this->create_subscription<geometry_msgs::msg::Vector3Stamped>(
+      "sim/wind_truth", 10, std::bind(&SimStateTranscription::wind_callback, this, std::placeholders::_1));
 
-    publisher_ = this->create_publisher<roscopter_msgs::msg::State>("state", 10);
+    roscopter_state_pub_ = this->create_publisher<roscopter_msgs::msg::State>("sim/roscopter/state", 10);
   }
 
 private:
-  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscription_;
-  rclcpp::Subscription<geometry_msgs::msg::Vector3Stamped>::SharedPtr
-    euler_sub_; // TODO is there going to be gitter between the times each of these collected?
+  rclcpp::Subscription<rosflight_msgs::msg::SimState>::SharedPtr sim_state_sub_;
+  rclcpp::Subscription<geometry_msgs::msg::Vector3Stamped>::SharedPtr wind_truth_sub_;
 
-  rclcpp::Publisher<State>::SharedPtr publisher_;
+  rclcpp::Publisher<roscopter_msgs::msg::State>::SharedPtr roscopter_state_pub_;
 
-  //TODO insert wind callback.
-  double wn = 0.0;
-  double we = 0.0;
-  double wd = 0.0;
+  double wn_ = 0.0;
+  double we_ = 0.0;
+  double wd_ = 0.0;
 
-  void publish_truth(const nav_msgs::msg::Odometry & msg)
+  void wind_callback(const geometry_msgs::msg::Vector3Stamped & msg)
   {
+    // Wind velocities in the inertial frame
+    wn_ = msg.vector.x;
+    we_ = msg.vector.y;
+    wd_ = msg.vector.z;
+  }
 
+  void publish_truth(const rosflight_msgs::msg::SimState & msg)
+  {
     roscopter_msgs::msg::State state;
 
+    // TODO: Should this use the timestamp of the incoming estimate?
     state.header.stamp = this->get_clock()->now();
     state.header.frame_id = 1; // Denotes global frame.
 
     state.initial_lat = 0.0;
-    state.initial_lon = 0.0; // TODO implement correct initial lat and lon
+    state.initial_lon = 0.0; // TODO: implement correct initial lat and lon
     state.initial_alt = 0.0;
 
-    state.position[0] = msg.pose.pose.position.x;
-    state.position[1] = msg.pose.pose.position.y;
-    state.position[2] = msg.pose.pose.position.z;
+    // Inertial NED frame
+    state.position[0] = msg.pose.position.x;
+    state.position[1] = msg.pose.position.y;
+    state.position[2] = msg.pose.position.z;
 
+    // Quaternion is from body to inertial
     Eigen::Quaternionf q;
-    q.w() = msg.pose.pose.orientation.w;
-    q.x() = msg.pose.pose.orientation.x;
-    q.y() = msg.pose.pose.orientation.y;
-    q.z() = msg.pose.pose.orientation.z;
+    q.w() = msg.pose.orientation.w;
+    q.x() = msg.pose.orientation.x;
+    q.y() = msg.pose.orientation.y;
+    q.z() = msg.pose.orientation.z;
 
     state.inclination = 0.0;
 
-    Eigen::Vector3f euler;
-    euler(0) = atan2(2.0 * (q.w() * q.x() + q.y() * q.z()),
+    // Equation B.1 in Small Unmanned Aircraft
+    state.phi = atan2(2.0 * (q.w() * q.x() + q.y() * q.z()),
                      pow(q.w(), 2) + pow(q.z(), 2) - pow(q.x(), 2) - pow(q.y(), 2));
-    euler(1) = asin(2.0 * (q.w() * q.y() - q.x() * q.z()));
-    euler(2) = atan2(2.0 * (q.w() * q.z() + q.x() * q.y()),
+    state.theta = asin(2.0 * (q.w() * q.y() - q.x() * q.z()));
+    state.psi = atan2(2.0 * (q.w() * q.z() + q.x() * q.y()),
                      pow(q.w(), 2) + pow(q.x(), 2) - pow(q.y(), 2) - pow(q.z(), 2));
 
-    state.phi = euler(0);
-    state.theta = euler(1);
-    state.psi = euler(2);
-
-    Eigen::Vector3f body_frame_velocity;
-    body_frame_velocity << msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z;
+    // Inertial linear velocities in body frame
+    Eigen::Vector3f body_frame_velocity(msg.twist.linear.x, msg.twist.linear.y, msg.twist.linear.z);
 
     // Rotate the velocity vector from the body frame to the inertial frame.
-    Eigen::Matrix3f Rv_v1, Rv1_v2, Rv2_b;
-    Rv2_b << 1, 0, 0, 0, cos(state.phi), sin(state.phi), 0, -sin(state.phi), cos(state.phi);
-    Rv1_v2 << cos(state.theta), 0, -sin(state.theta), 0, 1, 0, sin(state.theta), 0, cos(state.theta);
-    Rv_v1 << cos(state.psi), sin(state.psi), 0, -sin(state.psi), cos(state.psi), 0, 0, 0, 1;
+    Eigen::Vector3f inertial_frame_velocity = q * body_frame_velocity;
 
-    Eigen::Matrix3f Rb_i = (Rv2_b * Rv1_v2 * Rv_v1).transpose();
+    state.v_n = inertial_frame_velocity(0);
+    state.v_e = inertial_frame_velocity(1);
+    state.v_d = inertial_frame_velocity(2);
+    state.vg = inertial_frame_velocity.norm();
 
-    Eigen::Vector3f inertial_frame_velocity = Rb_i * body_frame_velocity;
+    // Angular velocities in body frame
+    state.p = msg.twist.angular.x;
+    state.q = msg.twist.angular.y;
+    state.r = msg.twist.angular.z;
 
-    state.v_n = body_frame_velocity(0);
-    state.v_e = body_frame_velocity(1);
-    state.v_d = body_frame_velocity(2);
-
-    state.vg = std::sqrt(pow(inertial_frame_velocity(0), 2)
-		       + pow(inertial_frame_velocity(1), 2)
-		       + pow(inertial_frame_velocity(2), 2));
-
-    state.p = msg.twist.twist.angular.x;
-    state.q = msg.twist.twist.angular.y;
-    state.r = msg.twist.twist.angular.z;
+    // TODO: Add the true biases to the state publisher or to a standalone_sensor publisher
 
     state.quat_valid = true;
 
-    state.quat[0] = msg.pose.pose.orientation.w;
-    state.quat[1] = msg.pose.pose.orientation.x;
-    state.quat[2] = msg.pose.pose.orientation.y;
-    state.quat[3] = msg.pose.pose.orientation.z;
+    state.quat[0] = msg.pose.orientation.w;
+    state.quat[1] = msg.pose.orientation.x;
+    state.quat[2] = msg.pose.orientation.y;
+    state.quat[3] = msg.pose.orientation.z;
 
-    publisher_->publish(state);
+    roscopter_state_pub_->publish(state);
   }
 };
+
+} // namespace roscopter_sim
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<gazebo_transcription>());
+  rclcpp::spin(std::make_shared<roscopter_sim::SimStateTranscription>());
   rclcpp::shutdown();
   return 0;
 }
